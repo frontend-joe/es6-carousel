133923514384f81b237f6995ec5f7fb4d7554c5b
frontend-joe <frontendjoe@outlook.com>
2023-07-02 13:46:40 +0100

    first commit

---
+++ carousel-1/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  speed: 500,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
---
+++ carousel-2/main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2,
+  speed: 600,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+  },
+});
